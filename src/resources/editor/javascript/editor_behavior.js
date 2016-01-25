c870416b668f12c9196e9df799ab1d62f481ae07
Teodoro Alcantara Aquino <teodoro641@hotmail.com>
2016-01-25 17:21:19 -0400

    Commented the editor behavior script
    Added functionality for moving the caret around when the user modifies
    word inside a span.

--- src/resources/editor/javascript/editor_behavior.js
+++ src/resources/editor/javascript/editor_behavior.js
@@ -2,30 +2,32 @@ function getSelectionStart() {
     var node = document.getSelection().anchorNode;
     return (node.nodeType == 3 ? node.parentNode : node);
 }
+
 window.onload = function(){
+    //content editable div where the user writes
     var d = document.getElementById("editor");
-    var preLastIndex = 0;
-    console.log(d.innerHTML);
-    var writing ="";
-    var count = 0;
-    var cero = 0;
+    var writing =""; //to save the words the user writes
     d.addEventListener("keypress",function(evt){
     var lastEl = d.lastChild;
-        if(evt.which === 8){
-            writing = writing.substring(0, writing.length-1);
-            //alert(writing);
-        }
-        else{
-            writing += String.fromCharCode(evt.which);
-           // alert(writing);
-        }
+    var carretControl = "";
+    // if the user press backspace
+    if(evt.which === 8){
+        //delete the last word from the writing variable
+        writing = writing.substring(0, writing.length-1);
+    }
+    else{
+        writing += String.fromCharCode(evt.which);
+    }
 
+    //if the user press space
     if(evt.which == 32){
-        // alert("ok angtes de la vaina");
+        //create a new span
         var newSpan = document.createElement('span');
+        //set the innerHTML of the span to the value of writing (which has the word the user just wrote)
         newSpan.innerHTML = writing.substring(0,writing.length);
-        //alert("este es el nuevo span con texto: "+newSpan.innerHTML);
+        //sets a click event in the new created span
         newSpan.onclick = function(){
+            //the code inside this function handler is not relevant, just text code
             var thisContentWords = this.innerHTML.split(" ");
             var editorChilds = Array.prototype.slice.call(d.children);
             console.log(this);
@@ -34,32 +36,65 @@ window.onload = function(){
             alert(this.innerHTML);
         }
 
+        //if the caret (the blinking thing from where the words suddenly appear)
+        //is not over a span element
         if (getSelectionStart().nodeName != "SPAN"){
-            //alert("nodo en lenghth -1: "+d.childNodes[d.childNodes.length - 2]);
+            //insert the word wraped in the new created span before the last child of the div
             d.insertBefore(newSpan, d.lastChild);
-            //alert("de first child: "+d.firstChild+" with text: "+d.firstChild.innerHTML);
-            //alert("de last child: "+d.lastChild);
+            //set the last child text to : '||'
+            //by the way the las child of the content will always be a textNode
             d.lastChild.textContent = "||";
-            //alert(window.focusNode);
 
-            var char = 3; // character at which to place caret  content.focus();
+            //get the position of the carrent
+            //which at this point is right at
+            //the end of the new created span
             var sel = window.getSelection();
+            //move the caret position 1 character ahead of it current position
             sel.collapse(d.lastChild, 1);
         }
+        //if the caret (the blinking thing from where the words suddenly appear)
+        //is over a span
         else if (getSelectionStart().nodeName == "SPAN"){
+            var writingCopy = writing;
+            //This is triggered when the caret is over a span and the user presses
+            //the space key the timer here is essential because without it the spans
+            //divition (when the user separate a word inside the spans the span will
+            //break in two diferent spans) will happend after the function was executed
+            //which wont allow the creation of span divition.
             window.setTimeout(function(){
+                //get the text of the span over which the caret is and split it around the spaces
                 var thisContentWords = getSelectionStart().innerHTML.split(" ");
-                var currentSpan = getSelectionStart();
-                var editorChilds = Array.prototype.slice.call(d.childNodes);
+                var currentSpan = getSelectionStart();  //the the span over which the caret is
+                //var editorChilds = Array.prototype.slice.call(d.childNodes);
+                //calls indexOf array function over the d.childNodes NodeLIst to get the index of the current span
                 var wordIndex = Array.prototype.indexOf.call(d.childNodes,getSelectionStart());
+                //since the user press the space bar in some place of the current span, the current span
+                //now has two words. the current span will get the first part of the text and the newSpan
+                //will take the second part of the text
                 currentSpan.innerHTML = thisContentWords[0]+" ";
                 newSpan.innerHTML = thisContentWords[1] + " ";
+                //the newSpan will be inserted before the element that has the caret on it
+                //at this point will be good to remember that the last element of the div will
+                //always be a textNode.
                 d.insertBefore(newSpan,d.childNodes[(Array.prototype.indexOf.call(d.childNodes,getSelectionStart())) + 1]);
-                console.log(wordIndex);
-            },200);
+                //alert("current: "+currentSpan.innerHTML);
+                //alert("writing: "+writing);
+                //alert("writing: "+writing.length);
+                //alert("writingCopy: "+writingCopy);
+                //alert("writingCopyleng: "+writingCopy.length);
+                //alert("newSpan: "+newSpan.innerHTML);
+                //alert("".length);
+
+                if (writingCopy.length === 1)
+                    window.getSelection().collapse(currentSpan, currentSpan.innerHTML.length - 1);
+                else{
+                    window.getSelection().collapse(newSpan, newSpan.innerHTML.length -1);
+                    writingCopy = "";
+                }
+            },100);
         }
-        writing = "";
-   }
+        writing = ""; //cleans the writing variable.
+       }
         //alert(document.body.innerHTML);
         //alert("Cono pero porque no te imprimes");
     });
